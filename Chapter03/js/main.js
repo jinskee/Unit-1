b6347fe86d2e9984d767fe5a601d6f12b2d0e7c6
jinskee <98485932+jinskee@users.noreply.github.com>
2022-02-09 15:24:55 -0600

    Changes Lab 3

--- Chapter03/js/main.js
+++ Chapter03/js/main.js
@@ -1,1 +1,12 @@
-//Javascript 
+function jsAjax(){
+    //Step 1: Define the data request
+    var request = new Request('data/MegaCities.geojson');
+    //Step 2: Define Fetch parameters
+    var init = {
+        method: 'GET'
+    }
+    //Step 3: use Fetch to retrieve the data
+    fetch(request, init)
+};
+
+window.onload = jsAjax();
